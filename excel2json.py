019814616b6b0eda4feb331e47b002e37d27ff0c
Nadh Y <nadh@budhil.com>
2018-08-24 15:57:18 +1000

    initial commit`

---
+++ excel2json.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import openpyxl
+import json
+import sys
+import os
+
+file = timetowait = sys.argv[1]
+filename, extension = os.path.splitext(file);
+
+wb = openpyxl.load_workbook(file)
+sheetnames = wb.sheetnames
+data = {}
+for sheet in sheetnames:
+    props = []
+    data[sheet] = []
+    sheet_obj = wb[sheet]
+    rows = sheet_obj.rows
+    for row in rows:
+        rowdata = {}
+        if len(props) is 0:
+            for cell in row:
+                props.append(cell.value)
+        else:
+            for index, cell in enumerate(row):
+                rowdata[props[index]] = cell.value
+            data[sheet].append(rowdata)
+
+with open(filename + ".json", "w") as outfile:
+    json.dump(data, outfile)
